bc6a664bfc0c0232cd2a7a593040ce9926705d7b
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-07-05 23:24:01 +0530

    changing the color of button using useState hook and style property

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
 const Button = () => {
-  const handleClick = (e) => {
-    console.log(e);
-    e.target.style.backgroundColor = color();
-  };
   const color = () => {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
@@ -12,9 +8,22 @@ const Button = () => {
     var RGBColor = "rgb(" + x + "," + y + "," + z + ")";
     return RGBColor;
   };
+  const [btn, setBtn] = React.useState(color());
+  const handleClick = (e) => {
+    console.log(e);
+
+    e.target.style.backgroundColor = color();
+  };
+  const handleClicktwo = () => {
+    setBtn(color());
+  };
+
   return (
     <div>
-      <button onClick={(e) => handleClick(e)}>change</button>
+      {/* <button onClick={(e) => handleClick(e)}>change</button> */}
+      <button style={{ backgroundColor: `${btn}` }} onClick={handleClicktwo}>
+        change
+      </button>
     </div>
   );
 };
